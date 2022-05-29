ba62740ef3f44226275575fe4aa30871918a16dd
ProgKea <hoschy01@googlemail.com>
2022-05-29 14:47:10 +0200

    made sound more quiet

--- src/init.c
+++ src/init.c
@@ -5,19 +5,22 @@ SDL_Renderer *renderer = NULL;
 
 void init_sdl2() {
   Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2043);
+  Mix_Volume(-1, MIX_MAX_VOLUME / 4);
+
   if (SDL_Init(SDL_INIT_EVERYTHING) > 0)
     fprintf(stderr, "Failed to init SDL: %s\n", SDL_GetError());
 
   if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
     fprintf(stderr, "Failed to init SDL_image: %s\n", SDL_GetError());
 
-  win = SDL_CreateWindow("bad game", SDL_WINDOWPOS_CENTERED,
-                         SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT,
-                         SDL_WINDOW_SHOWN);
+  win =
+      SDL_CreateWindow("bad game", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
   if (win == NULL)
     fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
 
-  renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+  renderer = SDL_CreateRenderer(
+      win, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
   if (renderer == NULL)
     fprintf(stderr, "Failed to create renderer: %s\n", SDL_GetError());
 
